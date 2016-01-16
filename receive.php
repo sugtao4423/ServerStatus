c3dcddbbf325d8fab35601a0b7e67a3c3a15030b
tao <sugtao4423@gmail.com>
2016-01-17 03:14:33 +0900

    ()が必要じゃないか・・・

--- receive.php
+++ receive.php
@@ -1,5 +1,5 @@
 <?php
-require_once './config.php';
+require_once('./config.php');
 $user = getUser();
 $password = getPassword();
 if(!isset($_SERVER['PHP_AUTH_USER'])){
--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
-require_once './config.php';
+require_once('./config.php');
 $user = getUser();
 $password = getPassword();
 $url = "https://${user}:${password}@sugtao4423.xyz/ServerStatus/receive.php";
