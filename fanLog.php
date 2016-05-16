2dd43e3876f19295e0f2f383298702b416daf682
tao <sugtao4423@gmail.com>
2016-05-17 02:38:54 +0900

    ログファイルが存在していない場合の処理＆すべて取得を実装

--- fanLog.php
+++ fanLog.php
@@ -1,13 +1,21 @@
 <?php
 $dbLocation = dirname(__FILE__) . "/database/fanLog/fanLog.db";
 
+if(!file_exists($dbLocation)){
+	echo '[]';
+	break;
+}
+
 if(isset($_GET['count']))
 	$count = $_GET['count'];
 else
 	$count = 10;
 
 $db = new SQLite3($dbLocation);
-$query = $db->query("select * from fanLog limit (select count(*) from fanLog)-${count}, ${count}");
+if($count === 'all')
+	$query = $db->query("select * from fanLog");
+else
+	$query = $db->query("select * from fanLog limit (select count(*) from fanLog)-${count}, ${count}");
 $arr = array();
 while ($rows = $query->fetchArray(SQLITE3_ASSOC))
 	array_push($arr, $rows);
