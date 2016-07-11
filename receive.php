720f869a03c67f13dd7391be69548d1daf869153
tao <sugtao4423@gmail.com>
2016-07-12 00:29:16 +0900

    データベースを一つに統合し、テーブルで管理するようにした
    サーバールームから換気扇を撤去したのでfanLogが不必要に。
    ※エアコン導入しました

--- receive.php
+++ receive.php
@@ -19,7 +19,7 @@ $json = json_decode($_POST[0], true);
 $name = $json['name'];
 
 $date = date('Y-m-d H:i');
-$location = dirname(__FILE__) . "/database/" . $name . "/";
+$db = new SQLite3(dirname(__FILE__) . "/database/" . $name . ".db");
 
 switch($name){
 	case xeon:
@@ -43,62 +43,42 @@ switch($name){
 		fanLog();
 		break;
 }
+$db->close();
 
 function sensors(){
-	global $date, $location, $json;
+	global $db, $date, $location, $json;
 	$core0 = $json["sensors"]["core0"];
 	$core1 = $json["sensors"]["core1"];
 	$core2 = $json["sensors"]["core2"];
 	$core3 = $json["sensors"]["core3"];
 
-	$db = new SQLite3($location . "sensors.db");
 	$db->exec("create table if not exists sensors(date, core0, core1, core2, core3)");
 	$db->exec("insert into sensors values('${date}', ${core0}, ${core1}, ${core2}, ${core3})");
-	$db->close();
 }
 
 function memory(){
-	global $date, $location, $json;
+	global $db, $date, $location, $json;
 	$used = $json["memory"]["used"];
 	$free = $json["memory"]["free"];
 	$swap = $json["memory"]["swap"];
 
-	$db = new SQLite3($location . "memory.db");
 	$db->exec("create table if not exists memory(date, used, free, swap)");
 	$db->exec("insert into memory values('${date}', ${used}, ${free}, ${swap})");
-	$db->close();
 }
 
 function process(){
-	global $date, $location, $json;
+	global $db, $date, $location, $json;
 	$process = $json["process"]["process"];
 	$zombie = $json["process"]["zombie"];
 
-	$db = new SQLite3($location . "process.db");
 	$db->exec("create table if not exists process(date, process, zombie)");
 	$db->exec("insert into process values('${date}', ${process}, ${zombie})");
-	$db->close();
 }
 
 function serverRoomTemp(){
-	global $date, $location, $json;
+	global $db, $date, $location, $json;
 	$temp = $json["temp"];
 
-	$db = new SQLite3($location . "temp.db");
 	$db->exec("create table if not exists temp(date, temp)");
 	$db->exec("insert into temp values('${date}', '${temp}')");
-	$db->close();
-}
-
-function fanLog(){
-	global $location, $json;
-	$status = $json["status"];
-	$date = $json["date"];
-	$time = $json["time"];
-	$temp = $json["temp"];
-
-	$db = new SQLite3($location . "fanLog.db");
-	$db->exec("create table if not exists fanLog(status, date, time, temp)");
-	$db->exec("insert into fanLog values('${status}', '${date}', '${time}', '${temp}')");
-	$db->close();
 }
