8eda67d9a9983241c1ab1a416a611126d479f3ce
sugtao4423 <sugtao4423@gmail.com>
2020-11-02 02:35:46 +0900

    authやsessionIdを直書きではなくログイン情報から取得するように
    それとめんどくさかったのでまたフルスクラッチした

--- README.md
+++ README.md
@@ -8,31 +8,37 @@ Zabbixのグラフを外部のページに貼り付けられるようにした�
 ```
 api.php?type=refreshServers
 ```
-実行すると同じディレクトリに`servers.json`が作成され、ZabbixのAPIを使用するユーザーが取得できるすべての`ホスト名` `ホストID` `グラフ名` `グラフID` `グラフタイプ`が保存される
+実行すると同じディレクトリに`zabbix.json`が作成される。
+
+`session`はグラフの取得に必要なセッションIDであり、  
+`hosts`はZabbixのAPIを使用するユーザーが取得できるすべての`ホスト名` `ホストID` `グラフ名` `グラフID` `グラフタイプ`の配列である。
 
 #### JSON Sample
 ```
-[
-  {
-    "hostid": 10198,
-    "name": "MyroomRaspi",
-    "graphs": [
-      {
-        "graphid": 825,
-        "graphtype": 0,
-        "name": "CPU jumps"
-      },
-      {
-        "graphid": 826,
-        "graphtype": 0,
-        "name": "CPU load"
-      }, ........
-    ]
-  }, ........
-]
+{
+  "session": "hogehoge",
+  "hosts": [
+    {
+      "hostid": 10198,
+      "name": "MyroomRaspi",
+      "graphs": [
+        {
+          "graphid": 825,
+          "graphtype": 0,
+          "name": "CPU jumps"
+        },
+        {
+          "graphid": 826,
+          "graphtype": 0,
+          "name": "CPU load"
+        }, ........
+      ]
+    }, ........
+  ]
+}
 ```
 
-## Get JSON
+## Get JSON of hosts array
 ```
 api.php
 ```
@@ -45,7 +51,7 @@ api.php?type=chart&graphtype=${GraphType}&graphid=${GraphId}
 ## Config
 api.php
 ```
-$ZBX_URL  = 'http://localhost.localdomain';
-$ZBX_AUTH = 'ZABBIX_AUTH';
-$ZBX_SID  = 'ZABBIX_SESSIONID';
+$ZBX_URL    = 'https://localhost.localdomain';
+$ZBX_USER   = 'ZABBIX_USER';
+$ZBX_PASSWD = 'ZABBIX_PASSWORD';
 ```
--- api.php
+++ api.php
@@ -1,160 +1,197 @@
 <?php
+
 declare(strict_types=1);
+
 /***** CONFIG *****/
-$ZBX_URL = 'http://localhost.localdomain';
-$ZBX_AUTH = 'ZABBIX_AUTH';
-$ZBX_SID = 'ZABBIX_SESSIONID';
+$ZBX_URL    = 'https://localhost.localdomain';
+$ZBX_USER   = 'ZABBIX_USER';
+$ZBX_PASSWD = 'ZABBIX_PASSWORD';
 /******************/
 
-$serverListPath = dirname(__FILE__) . '/servers.json';
+$jsonPath = __DIR__ . '/zabbix.json';
 
-switch($_GET['type']){
+switch ($_GET['type']) {
     default:
-        $response = getServers();
+        $response = getServers($jsonPath);
         break;
     case 'refreshServers':
-        $response = refreshServers();
+        $response = (new RefreshServers($ZBX_URL, $ZBX_USER, $ZBX_PASSWD, $jsonPath))->refresh();
         break;
     case 'chart':
-        $response = getChart();
+        $response = getChart($ZBX_URL, $jsonPath, $_GET['graphtype'], $_GET['graphid']);
         break;
 }
 echo $response;
 
-function getServers(): string{
-    global $serverListPath;
-    return file_get_contents($serverListPath);
+
+function getServers(string $jsonPath): string
+{
+    $json = file_get_contents($jsonPath);
+    $json = json_decode($json, true);
+    return json_encode($json['hosts'], JSON_UNESCAPED_UNICODE);
 }
 
-function refreshServers(): string{
-    function getZabbix(array $data): string{
-        $context = stream_context_create([
-            'http' => [
-                'method' => 'GET',
-                'header' => 'Content-Type: application/json-rpc',
-                'content' => json_encode($data)
-            ]
-        ]);
-        global $ZBX_URL;
-        return file_get_contents("${ZBX_URL}/api_jsonrpc.php", false, $context);
-    }
-
-    function sortAsName(array $arr): array{
-        $sort = [];
-        foreach($arr as $d){
-            $sort[] = $d['name'];
-        }
-        array_multisort($sort, SORT_ASC, SORT_NATURAL, $arr);
-        return $arr;
-    }
-
-    function getHosts(): array{
-        global $ZBX_AUTH;
-        $data = [
-            'jsonrpc' => '2.0',
-            'method' => 'host.get',
-            'params' => [
-                'output' => [
-                    'hostid',
-                    'name'
-                ]
-            ],
-            'auth' => $ZBX_AUTH,
-            'id' => 114514
-        ];
-        $zbx = getZabbix($data);
-        $json = json_decode($zbx, true)['result'];
-        $result = [];
-        foreach($json as $j){
-            $result[] = [
-                'hostid' => (int)$j['hostid'],
-                'name' => $j['name']
-            ];
-        }
-        return sortAsName($result);
-    }
-
-    function getGraphs(int $hostId): array{
-        global $ZBX_AUTH;
-        $data = [
-            'jsonrpc' => '2.0',
-            'method' => 'graph.get',
-            'params' => [
-                'output' => [
-                    'graphid',
-                    'graphtype',
-                    'name'
-                ],
-                'hostids' => $hostId
-            ],
-            'auth' => $ZBX_AUTH,
-            'id' => 810
-        ];
-        $zbx = getZabbix($data);
-        $graphs = json_decode($zbx, true)['result'];
-        $result = [];
-        foreach($graphs as $g){
-            if(preg_match('/^Network traffic on.*/', $g['name']) === 1 AND
-                preg_match('/^Network traffic on (eth|ens).*/', $g['name']) === 0){
-                continue;
-            }
-            $result[] = [
-                'graphid' => (int)$g['graphid'],
-                'graphtype' => (int)$g['graphtype'],
-                'name' => $g['name']
-            ];
-        }
-        return sortAsName($result);
-    }
-
-    $result = [];
-    $hosts = getHosts();
-    foreach($hosts as $h){
-        $graphs = getGraphs($h['hostid']);
-        $h['graphs'] = $graphs;
-        $result[] = $h;
-    }
-    $json = json_encode($result, JSON_UNESCAPED_UNICODE);
-
-    global $serverListPath;
-    $size = file_put_contents($serverListPath, $json, LOCK_EX);
-    return "finish.<br>${size} bytes.";
-}
-
-function getChart(): string{
-    $graphType = $_GET['graphtype'];
-    $graphId = $_GET['graphid'];
-    if(!is_numeric($graphType) OR !is_numeric($graphId)){
+function getChart(string $zabbixUrl, string $jsonPath, string $graphType, string $graphId): string
+{
+    if (!is_numeric($graphType) || !is_numeric($graphId)) {
         http_response_code(400);
         return 'Set graphtype and graphid as int';
     }
     $graphType = (int)$graphType;
     $graphId = (int)$graphId;
 
-    if($graphType < 0 OR $graphType > 2){
+    if ($graphType < 0 || $graphType > 2) {
         http_response_code(400);
-        return 'Does not correspond graphtype';
+        return 'Does not supported graphtype';
     }
 
-    global $ZBX_URL;
-    switch($graphType){
-        case 0:
-        case 1:
-            $chartUrl = "${ZBX_URL}/chart2.php?graphid=${graphId}&profileIdx=web.graphs.filter&from=now-2d&to=now";
-            break;
-
-        case 2:
-            $chartUrl = "${ZBX_URL}/chart6.php?graphid=${graphId}&profileIdx=web.graphs.filter&from=now-1h&to=now";
-            break;
+    if ($graphType == 2) {
+        $chartPath = 'chart6.php';
+    } else {
+        $chartPath = 'chart2.php';
     }
+    $chartUrl = "${zabbixUrl}/${chartPath}?graphid=${graphId}&profileIdx=web.graphs.filter&from=now-2d&to=now";
 
-    global $ZBX_SID;
+    $json = file_get_contents($jsonPath);
+    $json = json_decode($json, true);
+    $session = $json['session'];
+
     $context = stream_context_create([
         'http' => [
             'method' => 'GET',
-            'header' => "Cookie: zbx_sessionid=${ZBX_SID}"
+            'header' => "Cookie: zbx_session=${session}"
         ]
     ]);
     header('Content-type: image/png');
     return file_get_contents($chartUrl, false, $context);
+}
+
+class RefreshServers
+{
+    private $zabbixUrl;
+    private $zabbixUser;
+    private $zabbixPass;
+    private $jsonPath;
+
+    function __construct($zabbixUrl, string $zabbixUser, string $zabbixPass, string $jsonPath)
+    {
+        $this->zabbixUrl = $zabbixUrl;
+        $this->zabbixUser = $zabbixUser;
+        $this->zabbixPass = $zabbixPass;
+        $this->jsonPath = $jsonPath;
+    }
+
+    function refresh(): string
+    {
+        $auth = $this->getZabbixAuth();
+        $hosts = $this->getHosts($auth);
+        foreach ($hosts as &$host) {
+            $graphs = $this->getGraphs($auth, $host['hostid']);
+            $host['graphs'] = $graphs;
+        }
+
+        $session = $this->getSession();
+        $json = [
+            'session' => $session,
+            'hosts' => $hosts
+        ];
+        $json = json_encode($json, JSON_UNESCAPED_UNICODE);
+        $size = file_put_contents($this->jsonPath, $json, LOCK_EX);
+        return "finish.<br>${size} bytes.";
+    }
+
+    private function getSession(): string
+    {
+        $loginUrl = "{$this->zabbixUrl}/index.php";
+        $loginUrl .= "?name={$this->zabbixUser}&password={$this->zabbixPass}&enter=Sign%20in";
+        file_get_contents($loginUrl);
+        foreach ($http_response_header as $header) {
+            if (preg_match('/^Set-Cookie:.+?zbx_session=(.+?);/', $header, $m) === 1) {
+                return $m[1];
+            }
+        }
+    }
+
+    private function sendZabbixApi(string $method, array $params, ?string $auth): array
+    {
+        $context = stream_context_create([
+            'http' => [
+                'method' => 'GET',
+                'header' => 'Content-Type: application/json-rpc',
+                'content' => json_encode(
+                    [
+                        'jsonrpc' => '2.0',
+                        'method' => $method,
+                        'params' => $params,
+                        'auth' => $auth,
+                        'id' => 114514
+                    ]
+                )
+            ]
+        ]);
+        $json = file_get_contents("{$this->zabbixUrl}/api_jsonrpc.php", false, $context);
+        return json_decode($json, true);
+    }
+
+    private function getZabbixAuth(): string
+    {
+        $method = 'user.login';
+        $params = [
+            'user' => $this->zabbixUser,
+            'password' => $this->zabbixPass
+        ];
+        $json = $this->sendZabbixApi($method, $params, null);
+        return $json['result'];
+    }
+
+    private function getHosts(string $auth): array
+    {
+        $method = 'host.get';
+        $params = [
+            'output' => [
+                'hostid',
+                'name'
+            ],
+            'sortfield' => 'name'
+        ];
+        $json = $this->sendZabbixApi($method, $params, $auth);
+        $result = [];
+        foreach ($json['result'] as $host) {
+            $result[] = [
+                'hostid' => (int)$host['hostid'],
+                'name' => $host['name']
+            ];
+        }
+        return $result;
+    }
+
+    private function getGraphs(string $auth, int $hostId): array
+    {
+        $method = 'graph.get';
+        $params = [
+            'output' => [
+                'graphid',
+                'graphtype',
+                'name'
+            ],
+            'hostids' => $hostId,
+            'sortfield' => 'name'
+        ];
+        $json = $this->sendZabbixApi($method, $params, $auth);
+        $result = [];
+        foreach ($json['result'] as $graph) {
+            if (
+                preg_match('/^Network traffic on.*/', $graph['name']) === 1 &&
+                preg_match('/^Network traffic on (eth|ens).*/', $graph['name']) === 0
+            ) {
+                continue;
+            }
+            $result[] = [
+                'graphid' => (int)$graph['graphid'],
+                'graphtype' => (int)$graph['graphtype'],
+                'name' => $graph['name']
+            ];
+        }
+        return $result;
+    }
 }
